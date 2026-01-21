ad0cd3ba865266fff040ae1b0c5ff176096fce18
Rahul <rajendra.rahul87@gmail.com>
2026-01-21 23:18:27 +0000

    Modified WebTable2 testcase

--- tests/WebTable2.spec.ts
+++ tests/WebTable2.spec.ts
@@ -26,4 +26,49 @@ test('Web Table Static', async({page})=>{
     const rowTexts: string[] = await (row.nth(2).locator('td')).allInnerTexts();
     console.log('The second row text is', rowTexts);
 
+    //4) Content from a entire table excluding the header
+
+    const allData = await row.all(); //Get all row locators // all() returns array of locators
+    for(let data of allData.slice(1)){ // --> skip the header
+        const cellData:string[] = await data.locator('td').allInnerTexts();
+        console.log(cellData.join('\t'));
+    }
+
+    //5) Content from a entire table  
+    const allData1 = await row.allInnerTexts();
+
+    for(let data1 of allData1){
+        //const cellData1:string[] = await data1.allInnerTexts();
+        console.log(data1);
+    }
+
+    //6) Print book names where author is Mukesh
+
+    const bookName = [];
+    for(let data2 of allData.slice(1)){ // --> skip the header
+        const cellData2 = await data2.locator('td').allInnerTexts();
+        const author = cellData2[1];
+        const book = cellData2[0];
+
+        if(author === 'Mukesh'){
+            console.log(`${author} ---> ${book}`)
+            bookName.push(book);
+        }
+    }
+    console.log(bookName);
+    expect(bookName).toHaveLength(2); //Assertion
+
+    //7) Calculate the total price of all books
+    let totalPrice:number = 0; //Initializing totalPrice
+    for(let data2 of allData.slice(1)){ // --> skip the header
+        const cellData2 = await data2.locator('td').allInnerTexts();
+        const bookPrice = cellData2[3];
+        totalPrice+=parseInt(bookPrice);
+    }
+    console.log(`The total price of all books are ${totalPrice} `);
+    expect(totalPrice).toBe(7100);
+
+    
+
+
 })
