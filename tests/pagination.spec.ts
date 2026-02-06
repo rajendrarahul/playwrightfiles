e89c1f8f9c6d346f3b21a1db4a12802086374dd1
Rahul <rajendra.rahul87@gmail.com>
2026-02-06 16:34:26 +0000

    Added more tests in Pagination test case

--- tests/pagination.spec.ts
+++ tests/pagination.spec.ts
@@ -27,3 +27,60 @@ test('Read all data from all pages',async({page})=>{
         }
     }
 })
+
+test('Filter Verification',async({page})=>{
+
+    await page.setViewportSize({width:1920,height:1080});
+    await page.goto('https://datatables.net/examples/basic_init/zero_configuration.html');
+
+    //Locate the dropdown element and select the filter value by label.
+    const dropDown: Locator = await page.locator("#dt-length-0");
+    await dropDown.selectOption({label:'50'});
+
+    //Approach1: using .all() method
+
+    const tableRows = await page.locator("#example tbody tr").all()
+    expect(tableRows.length).toBe(50);
+
+    //Approach2: without using .all() method, and use toHaveCount() assertion
+
+    const tableRows2 = await page.locator("#example tbody tr")
+    expect(tableRows2).toHaveCount(50);
+})
+
+test.only('Search Data',async({page})=>{
+
+    await page.setViewportSize({width:1920,height:1080});
+    await page.goto('https://datatables.net/examples/basic_init/zero_configuration.html');
+
+    //Locate the search box and enter the search text.
+    await page.locator('#dt-search-0').fill('Soniya Frost');
+    await page.waitForTimeout(5000);
+
+    //After searching get all the rows with the search results.
+    const Rows = await page.locator("#example tbody tr");
+    const allrows = await Rows.all();
+    const rowTexts = await Rows.allInnerTexts();
+    //Checking for any empty record message. Better than checking for the number of rows displayed.
+    //const isNotFound =  rowTexts.includes('No matching records found')
+    const isNotFound = rowTexts.some(text => text.includes('No matching records found'))
+    //Condition check for empty message presence.
+    if(isNotFound){
+        console.log('The record not found');  
+    }
+    else{
+        let matchFound = false;
+        for(let ro of allrows){
+            const text = await ro.innerText();
+            if(text.includes('Soniya Frost')) {
+                console.log('The record exists');
+                matchFound = true;
+                break;
+            }
+        }
+        //expect(matchFound).toBeTruthy(); 
+        //expect(matchFound).toBe(true);
+
+        
+    }
+})
